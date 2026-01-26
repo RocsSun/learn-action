2bc23389dcaaba9cbb33a82433f73beddb574ae4
Roc Sun <710989028@qq.com>
2026-01-26 15:19:43 +0800

    feat: update test message to include version v0.3.3

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("update message for test ci");
-    println!("update message for test ci on version v0.3.2");
+    println!("update message for test ci on version v0.3.3");
     println!("Hello, world!");
     println!("This message is used for testing purposes.");
 }
